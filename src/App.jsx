5d76471ed2787dfad66e23d3a28bfd466898547c
wmfsdev <117413083+wmfsdev@users.noreply.github.com>
2023-12-05 22:38:24 +0000

    router setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import { Link } from "react-router-dom";
 
-
-const App = (props) => {
-  return <div>{props.name}</div>;
-};
-
-App.propTypes = {
-  name: PropTypes.string,
-};
-
-App.defaultProps = {
-  name: 'Zach',
+const App = () => {
+  return (
+    <div>
+      <h1>Hello from the main page of the app!</h1>
+      <p>Here are some examples of links to other pages</p>
+      <nav>
+        <ul>
+          <li>
+            <Link to="profile">Profile page</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default App;
+
+
+
+//import PropTypes from 'prop-types';
+
+// const App = (props) => {
+//   return <div>{props.name}</div>;
+// };
+
+// App.propTypes = {
+//   name: PropTypes.string,
+// };
+
+// App.defaultProps = {
+//   name: 'Zach',
+// };
+
+// export default App;
 
 // export const CustomButton = ({ onClick }) => {
 //   return (
